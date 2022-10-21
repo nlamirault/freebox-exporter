a0f24a6648791c6251e0a41c3e68291b370d7fbd
Nicolas Lamirault <nicolas.lamirault@gmail.com>
2022-10-21 16:21:53 +0200

    Update: clean
    Signed-off-by: Nicolas Lamirault <nicolas.lamirault@gmail.com>

--- main.go
+++ main.go
@@ -19,15 +19,11 @@ import (
 )
 
 const (
-	application = "freebox-exporter"
+	EXPORTER = "freebox-exporter"
 )
 
 var (
-	webConfig = webflag.AddFlags(kingpin.CommandLine, ":9091")
-	// listen    = kingpin.Flag(
-	// 	"address",
-	// 	"Address to listen on for web interface and telemetry.",
-	// ).OverrideDefaultFromEnvar("FREEBOX_EXPORTER_ADDR").Default(":9091").String()
+	webConfig  = webflag.AddFlags(kingpin.CommandLine, ":9091")
 	metricPath = kingpin.Flag(
 		"web.telemetry-path",
 		"Path under which to expose metrics.",
@@ -54,51 +50,16 @@ var (
 	).OverrideDefaultFromEnvar("FREEBOX_EXPORTER_DEBUG").Default("false").Bool()
 )
 
-// func usage() {
-// 	fmt.Fprintf(flag.CommandLine.Output(),
-// 		"Usage: %s [options] <api_token_file>\n"+
-// 			"\n"+
-// 			"api_token_file: file to store the token for the API\n"+
-// 			"\n"+
-// 			"options:\n",
-// 		os.Args[0])
-// 	flag.PrintDefaults()
-// }
-
 func main() {
 	promlogConfig := &promlog.Config{}
 	flag.AddFlags(kingpin.CommandLine, promlogConfig)
-	kingpin.Version(version.Print("freebox-exporter"))
+	kingpin.Version(version.Print(EXPORTER))
 	kingpin.HelpFlag.Short('h')
 	kingpin.Parse()
+
 	logger := promlog.New(promlogConfig)
-
-	level.Info(logger).Log("msg", "Starting freebox-exporter", "version", version.Info())
-	level.Debug(logger).Log("msg", "Build context", "context", version.BuildContext())
-
-	// flag.Usage = usage
-	// debugPtr := flag.Bool("debug", false, "enable the debug mode")
-	// hostDetailsPtr := flag.Bool("hostDetails", false, "get details about the hosts connected to wifi and ethernet. This increases the number of metrics")
-	// httpDiscoveryPtr := flag.Bool("httpDiscovery", false, "use http://mafreebox.freebox.fr/api_version to discover the Freebox at the first run (by default: use mDNS)")
-	// apiVersionPtr := flag.Int("apiVersion", 0, "Force the API version (by default use the latest one)")
-	// // listenPtr := flag.String("listen", ":9091", "listen to address")
-	// flag.Parse()
-
-	// args := flag.Args()
-	// if len(args) < 1 {
-	// 	fmt.Fprintf(flag.CommandLine.Output(), "ERROR: api_token_file not defined\n")
-	// 	usage()
-	// 	os.Exit(1)
-	// } else if len(args) > 1 {
-	// 	fmt.Fprintf(flag.CommandLine.Output(), "ERROR: too many arguments\n")
-	// 	usage()
-	// 	os.Exit(1)
-	// }
-	// if *debugPtr {
-	// 	log.InitDebug()
-	// } else {
-	// 	log.Init()
-	// }
+	level.Info(logger).Log("msg", fmt.Sprintf("Starting %s", EXPORTER), "version", version.Info())
+	level.Debug(logger).Log("msg", "Build context", version.BuildContext())
 
 	discoveryMode := fbx.FreeboxDiscoveryMDNS
 	if *discovery {
@@ -131,7 +92,6 @@ func main() {
 		w.WriteHeader(http.StatusOK)
 		fmt.Fprintf(w, "OK")
 	})
-	// http.Handle("/metrics", promhttp.Handler())
 	http.Handle(*metricPath,
 		promhttp.InstrumentMetricHandler(
 			prometheus.DefaultRegisterer,
@@ -146,16 +106,7 @@ func main() {
 		),
 	)
 
-	// level.Info(logger).Log("msg", "Starting HTTP server", "port", listen)
-	// log.Error.Println(http.ListenAndServe(*listen, nil))
-	// srv := &http.Server{Addr: *listen}
-	// if err := web.ListenAndServe(srv, webConfig, logger); err != nil {
-	// 	level.Error(logger).Log("msg", "Error starting HTTP server", "err", err)
-	// 	os.Exit(1)
-	// }
-
 	server := &http.Server{}
-
 	if err := web.ListenAndServe(server, webConfig, logger); err != nil {
 		log.Fatal(err)
 	}
